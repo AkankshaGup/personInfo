e048150a8a834f1c35d59e3d8f0c427c6df2ed40
unknown <akankshagupta9210@gmail.com>
2020-07-31 22:09:07 -0700

    commit

--- Desktop/akankshaProject/crystal-machine-test/src/reactStore/store.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {createStore,applyMiddleware} from "redux";
-import {UserInfo} from "../reducer/userInfo.js";
-import thunk from "redux-thunk"
-
- 
-export default  createStore(UserInfo,  applyMiddleware(thunk) );
- 
